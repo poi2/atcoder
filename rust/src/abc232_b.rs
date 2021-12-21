50c356d385bd575914801c1db2300cc9f3d86dcc
poi2 <daisuke.ito.cs@gmail.com>
2021-12-21 10:29:26 +0900

    ADD ABC232 B by Rust

---
+++ rust/src/abc232_b.rs
@@ -0,0 +1,42 @@
+use proconio::input;
+use itertools::Itertools;
+
+fn get_index(target: &str, chars: &Vec<&str>) -> i32 {
+    let (idx, _) = chars
+        .iter()
+        .find_position(|&&c| c == target )
+        .unwrap();
+    (idx as i32) % 26
+}
+
+fn main() {
+    input! {
+        s: String,
+        t: String,
+    };
+    let slist: Vec<&str> = s.split("").collect();
+    let tlist: Vec<&str> = t.split("").collect();
+
+    let chars = vec![
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z"
+    ];
+
+    let i = 1;
+    let diff = (
+        get_index(slist[i], &chars) - get_index(tlist[i], &chars) + 26
+    ) % 26;
+
+    let mut ans = "Yes";
+    for i in 0..slist.len() {
+        if slist[i] != "" {
+            let sidx = get_index(slist[i], &chars);
+            let tidx = get_index(tlist[i], &chars);
+            if ((sidx - tidx + 26) % 26) != diff {
+                ans = "No";
+            }
+        }
+    }
+    println!("{}", ans);
+}
