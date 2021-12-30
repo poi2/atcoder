4bc31d07cde377eca0a0cf7f37d899a783f9fda0
poi2 <daisuke.ito.cs@gmail.com>
2021-12-30 17:26:07 +0900

    ADD ABC233 C with 直積集合

--- rust/src/abc233_c.rs
+++ rust/src/abc233_c.rs
@@ -1,37 +1,60 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 use proconio::input;
+use itertools::Itertools;
 use itertools::iproduct;
 
-fn dfs(i: usize, prod: usize, goal: usize, bag_size: usize, aij: &Vec<Vec<usize>>, ans: &mut usize) {
-    if i == bag_size {
-        if prod == goal {
-            *ans += 1;
-        }
-        return;
-    }
-    for j in &aij[i] {
-        if prod <= goal / j {
-            dfs(i + 1, prod * j, goal, bag_size, aij, ans);
-        }
-    }
-}
+// fn dfs(i: usize, prod: usize, goal: usize, bag_size: usize, aij: &Vec<Vec<usize>>, ans: &mut usize) {
+//     if i == bag_size {
+//         if prod == goal {
+//             *ans += 1;
+//         }
+//         return;
+//     }
+//     for j in &aij[i] {
+//         if prod <= goal / j {
+//             dfs(i + 1, prod * j, goal, bag_size, aij, ans);
+//         }
+//     }
+// }
 
 fn main() {
+    // input! {
+    //     bag_size: usize,
+    //     goal: usize,
+    // }
+    // let mut aij: Vec<Vec<usize>> = vec![vec![]; bag_size];
+
+    // for i in 0..bag_size {
+    //     input! {
+    //         l: usize,
+    //         an: [usize; l],
+    //     }
+    //     aij[i] = an;
+    // }
+    // let mut ans = 0;
+
+    // dfs(0, 1, goal, bag_size, &aij, &mut ans);
+    // println!("{}", ans);
+
     input! {
         bag_size: usize,
-        goal: usize,
+        goal: u128,
     }
-    let mut aij: Vec<Vec<usize>> = vec![vec![]; bag_size];
+    let mut aij= vec![];
 
-    for i in 0..bag_size {
+    for _ in 0..bag_size {
         input! {
-            l: usize,
-            an: [usize; l],
+            l: u128,
+            an: [u128; l],
         }
-        aij[i] = an;
+        aij.push(an);
     }
-    let mut ans = 0;
 
-    dfs(0, 1, goal, bag_size, &aij, &mut ans);
+    let ans = aij
+        .iter()
+        .multi_cartesian_product()
+        .map(|prod_vec| prod_vec.iter().fold(1, |acc, &&u| acc * u))
+        .filter(|&n| n == goal)
+        .count();
     println!("{}", ans);
 }
