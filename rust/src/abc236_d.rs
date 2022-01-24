f5986ec2bf01ce0ed71ce813657e7fae98458297
poi2 <daisuke.ito.cs@gmail.com>
2022-01-24 10:18:07 +0900

    ADD ABC236 D ビット演算による used 管理
    used を vec で管理すると、
    
    - vec copy trait ないエラー
    - ライフタイムの問題
    - used をリセットする処理
    
    が発生して難しい。
    ビット演算で used を回避し、上記の問題の回避を行った。

--- rust/src/abc236_d.rs
+++ rust/src/abc236_d.rs
@@ -13,38 +13,66 @@ fn main() {
         input! { x: [usize; 2 * n - 1 - i] }
         chems.push(x);
     }
-    let mut used = vec![false; 2 * n];
-    let ans = solve(0, 0, 0, &mut used, n, &chems);
+    // let mut used = vec![false; 2 * n];
+    // let ans = old_solve(0, 0, 0, &mut used, n, &chems);
+    let ans = solve(0,  0, 0, n, &chems);
 
     println!("{}", ans);
 }
 
-fn solve(i: usize, team_count: usize, acc: usize, used: &mut Vec<bool> ,max_team: usize, chems: &Vec<Vec<usize>>) -> usize {
-    if team_count == max_team {
+fn to_bit(i: usize) -> usize { 1 << i }
+fn is_used(used: usize, i: usize) -> bool {
+    let bit = to_bit(i);
+    used & bit == bit
+}
+fn set_used(used: usize, i: usize, j: usize) -> usize { used | to_bit(i) | to_bit(j) }
+
+fn solve(i: usize, acc: usize, used: usize ,max_team: usize, chems: &Vec<Vec<usize>>) -> usize {
+    if used == to_bit(2 * max_team) - 1 {
         return acc;
     }
 
-    if used[i] == true {
-        return solve(i + 1, team_count, acc, used, max_team, chems);
-    } else {
-        used[i] = true;
-        let mut max = 0;
-        for j in (i + 1)..(max_team * 2) {
-            if used[j] == false {
-                used[j] = true;
-                let ret = solve(
-                    i + 1,
-                    team_count + 1,
-                    acc ^ chems[i][j - i - 1],
-                    used,
-                    max_team,
-                    chems
-                );
-                max = max.max(ret);
-                used[j] = false
-            }
-        }
-        used[i] = false;
-        return max;
+    if is_used(used, i) {
+        return solve(i + 1, acc, used, max_team, chems);
     }
+    let mut max = 0;
+    for j in (i + 1)..(max_team * 2) {
+        if !is_used(used, j) {
+            let next_acc = acc ^ chems[i][j - i - 1];
+            let next_used = set_used(used, i, j);
+            let ret = solve(i + 1, next_acc, next_used, max_team, chems);
+            max = max.max(ret);
+        }
+    }
+    return max;
 }
+
+// fn old_solve(i: usize, team_count: usize, acc: usize, used: &mut Vec<bool> ,max_team: usize, chems: &Vec<Vec<usize>>) -> usize {
+//     if team_count == max_team {
+//         return acc;
+//     }
+
+//     if used[i] == true {
+//         return old_solve(i + 1, team_count, acc, used, max_team, chems);
+//     } else {
+//         used[i] = true;
+//         let mut max = 0;
+//         for j in (i + 1)..(max_team * 2) {
+//             if used[j] == false {
+//                 used[j] = true;
+//                 let ret = old_solve(
+//                     i + 1,
+//                     team_count + 1,
+//                     acc ^ chems[i][j - i - 1],
+//                     used,
+//                     max_team,
+//                     chems
+//                 );
+//                 max = max.max(ret);
+//                 used[j] = false
+//             }
+//         }
+//         used[i] = false;
+//         return max;
+//     }
+// }
