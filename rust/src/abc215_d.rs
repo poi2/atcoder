4082b1ffa051415e7cd163c24cb956d0b3320a73
poi2 <daisuke.ito.cs@gmail.com>
2022-02-17 11:22:53 +0900

    ADD エラトステネスの篩

--- rust/src/abc215_d.rs
+++ rust/src/abc215_d.rs
@@ -61,3 +61,65 @@ fn prime_factorization(n: usize) -> Vec<(usize, usize)> {
 
     return ans;
 }
+
+
+// NOTE エラトステネスの篩
+pub struct Sieve {
+	sieve: Vec<u64>,
+	n: usize,
+}
+impl Sieve {
+	const BIT_LENGTH: usize = std::mem::size_of::<u64>() * 8;
+
+	pub fn new(n: usize) -> Self {
+		let base = (1..Self::BIT_LENGTH).step_by(2).fold(0, |r, i| r | 1 << i);
+		let mut sieve = vec![base; n / Self::BIT_LENGTH + 1];
+		sieve[0] ^= 0b0110;
+		for p in (3..).step_by(2).take_while(|&i| i * i < n) {
+			if sieve[p / Self::BIT_LENGTH] & (1 << p % Self::BIT_LENGTH) != 0 {
+				for q in (p * p..=n).step_by(2 * p) {
+					sieve[q / Self::BIT_LENGTH] &= std::u64::MAX ^ (1 << q % Self::BIT_LENGTH);
+				}
+			}
+		}
+		let r = n % Self::BIT_LENGTH;
+		*sieve.last_mut().unwrap() &= ((1 << r) - 1) | 1 << r;
+		Self { sieve, n }
+	}
+
+	pub fn is_prime<T>(&self, p: T) -> bool
+	where
+		T: std::convert::TryInto<usize>,
+	{
+		if let Ok(p) = p.try_into() {
+			assert!(p <= self.n);
+			self.sieve[p / Self::BIT_LENGTH] & (1 << p % Self::BIT_LENGTH) != 0
+		} else {
+			false
+		}
+	}
+
+	pub fn primes<'a, T>(&'a self) -> impl 'a + std::iter::Iterator<Item = T>
+	where
+		T: std::convert::TryFrom<usize>,
+	{
+		let mut it = self.sieve.iter();
+		let mut bits = 0;
+		let mut p = 0;
+		std::iter::from_fn(move || {
+			while bits == 0 {
+				if let Some(next) = it.next().cloned() {
+					bits = next;
+					p += Self::BIT_LENGTH;
+				} else {
+					return None;
+				}
+			}
+			let lsb = bits & 0u64.wrapping_sub(bits);
+			bits ^= lsb;
+			let q = (p - Self::BIT_LENGTH) + lsb.trailing_zeros() as usize;
+
+			T::try_from(q).ok()
+		})
+	}
+}
